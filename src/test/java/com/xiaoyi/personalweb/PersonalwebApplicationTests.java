8cf69c5089b3765254de7f0fa0e1d3bffa26994c
extends <7646687+extendsx@user.noreply.gitee.com>
2022-08-02 11:22:38 +0800

    初始化项目

---
+++ src/test/java/com/xiaoyi/personalweb/PersonalwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoyi.personalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
