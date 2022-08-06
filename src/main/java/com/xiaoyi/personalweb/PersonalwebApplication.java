479db6eb9cf6285bf79f25c14de3296209ac8b82
extends <7646687+extendsx@user.noreply.gitee.com>
2022-08-07 07:37:16 +0800

    添加依赖、修改配置文件

--- src/main/java/com/xiaoyi/personalweb/PersonalwebApplication.java
+++ src/main/java/com/xiaoyi/personalweb/PersonalwebApplication.java
@@ -1,9 +1,11 @@
 package com.xiaoyi.personalweb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xiaoyi.personalweb.mapper")
 public class PersonalwebApplication {
 
     public static void main(String[] args) {
