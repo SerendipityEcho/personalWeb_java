c3b9d46c2598af9cb1a66fe2899acc064989d22c
extends <7646687+extendsx@user.noreply.gitee.com>
2022-09-02 15:58:53 +0800

    用户信息更改

---
+++ src/main/java/com/xiaoyi/personalweb/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.xiaoyi.personalweb.controller;
+
+import com.xiaoyi.personalweb.annotation.CurrentUser;
+import com.xiaoyi.personalweb.entity.Result;
+import com.xiaoyi.personalweb.entity.User;
+import com.xiaoyi.personalweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public Result getUserInfo(@CurrentUser User user){
+//        List list = userService.queryByList(user);
+        return Result.getSuccessResult(user);
+    }
+
+    @GetMapping("/list")
+    public Result getUserList(User user){
+        List userList = userService.queryByList(user);
+        return Result.getSuccessResult(userList);
+    }
+}
