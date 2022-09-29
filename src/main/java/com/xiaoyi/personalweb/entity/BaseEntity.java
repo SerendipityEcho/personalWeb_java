2e0cfb0a1588f3991157806845a9003266e3f1a9
extends <7646687+extendsx@user.noreply.gitee.com>
2022-09-29 15:41:15 +0800

    添加逻辑删除

--- src/main/java/com/xiaoyi/personalweb/entity/BaseEntity.java
+++ src/main/java/com/xiaoyi/personalweb/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.xiaoyi.personalweb.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -14,10 +15,16 @@ public class BaseEntity implements Serializable {
 
     @TableField("create_time")
     private Date createTime ;    //日志-创建时间
+
     @TableField(value = "update_by",fill = FieldFill.INSERT_UPDATE)
     private String updateUserId; //日志-最后修改执行人
+
     @TableField("update_time")
     private Date updateTime;     //日志-最后修改时间
+
+    @TableLogic
+    @TableField("delete_status")
+    private Integer deleteStatus; // 删除状态，实现逻辑删除
 
     public String getCreateUserId() {
         return createUserId;
