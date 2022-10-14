ffd58a990cc608ee92606f992f05f7012cab125a
extends <7646687+extendsx@user.noreply.gitee.com>
2022-10-14 16:51:39 +0800

    添加状态码

---
+++ src/main/java/com/xiaoyi/personalweb/util/ResultBody.java
@@ -0,0 +1,82 @@
+package com.xiaoyi.personalweb.util;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResultBody {
+
+    /**
+     * 响应代码
+     */
+    private String code;
+
+    /**
+     * 响应消息
+     */
+    private String message;
+
+    /**
+     * 响应结果
+     */
+    private Object result;
+
+    /**
+     * 成功
+     */
+    public static ResultBody success() {
+        return success(null);
+    }
+
+    /**
+     * 成功
+     */
+    public static ResultBody success(Object data) {
+        ResultBody rb = new ResultBody();
+        rb.setCode(CommonEnum.SUCCESS.getResultCode());
+        rb.setMessage(CommonEnum.SUCCESS.getResultMsg());
+        rb.setResult(data);
+        return rb;
+    }
+
+    /**
+     * 失败
+     */
+    public static ResultBody error(CommonEnum errorInfo) {
+        ResultBody rb = new ResultBody();
+        rb.setCode(errorInfo.getResultCode());
+        rb.setMessage(errorInfo.getResultMsg());
+        rb.setResult(null);
+        return rb;
+    }
+
+    /**
+     * 失败
+     */
+    public static ResultBody error(String code, String message) {
+        ResultBody rb = new ResultBody();
+        rb.setCode(code);
+        rb.setMessage(message);
+        rb.setResult(null);
+        return rb;
+    }
+
+    /**
+     * 失败
+     */
+    public static ResultBody error( String message) {
+        ResultBody rb = new ResultBody();
+        rb.setCode("-1");
+        rb.setMessage(message);
+        rb.setResult(null);
+        return rb;
+    }
+
+    @Override
+    public String toString() {
+        return JSONObject.toJSONString(this);
+    }
+
+}
