905c4a1c9959620bd124f481b684d55150962921
extends <7646687+extendsx@user.noreply.gitee.com>
2022-10-18 08:52:23 +0800

    修改商品接口

--- src/main/java/com/xiaoyi/personalweb/service/impl/ProductServiceImpl.java
+++ src/main/java/com/xiaoyi/personalweb/service/impl/ProductServiceImpl.java
@@ -1,9 +1,11 @@
 package com.xiaoyi.personalweb.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xiaoyi.personalweb.entity.Price;
 import com.xiaoyi.personalweb.entity.Product;
 import com.xiaoyi.personalweb.entity.Result;
 import com.xiaoyi.personalweb.mapper.ProductMapper;
+import com.xiaoyi.personalweb.service.PriceService;
 import com.xiaoyi.personalweb.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,13 +17,30 @@ import java.util.List;
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductMapper productMapper;
+    private final PriceService priceService;
+
     @Autowired
-    private ProductMapper productMapper;
+    public ProductServiceImpl(PriceService priceService, ProductMapper productMapper) {
+        this.priceService = priceService;
+        this.productMapper = productMapper;
+    }
 
     @Override
     public Result addProduct(Product product) {
+        // 添加商品会默认在价格表中添加一条对应的价格数据
         int row = productMapper.insert(product);
-        return new Result(row,row > 0);
+        // 在商品插入成功后mybatis会自动在实体类中插入该条数据的id
+        // 插入默认的价格列表
+        Price price = new Price();
+        price.setProductId(product.getId());
+        price.setPrice(product.getPrice());
+        Result priceRow = priceService.addPrice(price);
+        // 当两个都插入成功时返回成功，否则插入失败
+        if (priceRow.isSuccess() && row > 0){
+            return new Result(row,true);
+        }
+        return new Result(row,false);
     }
 
     @Override
@@ -41,6 +60,9 @@ public class ProductServiceImpl implements ProductService {
         QueryWrapper<Product> wrapper = new QueryWrapper<>();
         wrapper.like(product.getName() != null,"name", product.getName());
         List<Product> productList = productMapper.selectList(wrapper);
+        // 分页功能 暂时注掉，需要分页时使用
+//        Page<Product> page = new Page<>();
+//        Page<Product> productPage = productMapper.selectPage(page, wrapper);
         return Result.getSuccessResult(productList);
     }
 }
