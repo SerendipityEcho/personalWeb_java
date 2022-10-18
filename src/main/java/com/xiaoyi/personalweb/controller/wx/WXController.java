b5a6bce385da1f1fc207e0345d7360860b77a461
extends <7646687+extendsx@user.noreply.gitee.com>
2022-10-18 08:56:34 +0800

    修改注释

--- src/main/java/com/xiaoyi/personalweb/controller/wx/WXController.java
+++ src/main/java/com/xiaoyi/personalweb/controller/wx/WXController.java
@@ -29,12 +29,10 @@ public class WXController extends WeixinSupport {
      */
     @GetMapping("/bind")
     public String wxAuth(HttpServletRequest request){
-        /**
-         * 开发者通过检验signature对请求进行校验（下面有校验方式）。
-         * 若确认此次GET请求来自微信服务器，请原样返回echostr参数内容，
-         * 则接入生效，成为开发者成功，否则接入失败。
-         * 开发者提交信息后，微信服务器将发送GET请求到填写的服务器地址URL上.
-         */
+        // 开发者通过检验signature对请求进行校验（下面有校验方式）。
+        // 若确认此次GET请求来自微信服务器，请原样返回echostr参数内容，
+        // 则接入生效，成为开发者成功，否则接入失败。
+        // 开发者提交信息后，微信服务器将发送GET请求到填写的服务器地址URL上.
         if (isLegal(request)) {
             //调用菜单创建接口
             //绑定微信服务器成功
